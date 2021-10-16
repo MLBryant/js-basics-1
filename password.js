e1edbd2b8d5df808485ec53c147323f95c00c5fd
Mason Bryant <mlbryant6@gmail.com>
2021-10-15 21:00:57 -0500

    password.js finalized

--- password.js
+++ password.js
@@ -5,16 +5,59 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome!');
-reader.question("Please enter your password for validation: ", function(input){
-	tokens = input;
+console.log(`  _______             __                  __ 
+|   _   .-----.-----|__.-----.--.--.----|  |
+|.  1   |  _  |     |  |  _  |  |  |   _|__|
+|.  _   |_____|__|__|  |_____|_____|__| |__|
+|:  1    \\         |___|                    
+|::.. .  /                                  
+\`-------'       `)
 
-    if (tokens.length >= 10) {
+console.log('Welcome!');
+reader.question("Please enter your password for validation \nPasswords must be at least 10 characters long and contain Upper and Lower case letters and numbers \nEnter password: ", function(input){
+	tokens = input;
+    character = '';
+    numCheck = false;
+    upCaseCheck = false;
+    lowCaseCheck = false;
+    
+    for (let i = 0; i < tokens.length; i++) {
+        character = tokens.charAt(i);
+        if (!isNaN(character * 1)) {
+            numCheck = true;
+        } else { 
+           if (character === character.toUpperCase()) {
+            upCaseCheck = true;
+           }
+           if (character === character.toLowerCase()) {
+               lowCaseCheck = true;
+           }
+        }
+    }
+
+    if (tokens.length >= 10 && numCheck == true && upCaseCheck == true && lowCaseCheck == true) {
         console.log('Password validated, success!');
+console.log(`  _______                              __         __ 
+|   _   .-----.-----.-----.----.---.-|  |_.-----|  |
+|.  1___|  _  |     |  _  |   _|  _  |   _|__ --|__|
+|.  |___|_____|__|__|___  |__| |___._|____|_____|__|
+|:  1   |           |_____|                         
+|::.. . |                                           
+\`-------'`);
+        
     } else {
         console.log('Validation failed =[');
+console.log(`  _______                _______             __       
+|       .----.--.--.   |   _   .-----.---.-|__.-----.
+|.|   | |   _|  |  |   |.  1   |  _  |  _  |  |     |
+\`-|.  |-|__| |___  |   |.  _   |___  |___._|__|__|__|
+  |:  |      |_____|   |:  |   |_____|               
+  |::.|                |::.|:. |                     
+  \`---'                \`--- ---'`);
+      
     }
 
     reader.close()
 
 })
+
